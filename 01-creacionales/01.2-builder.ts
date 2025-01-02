3716fe9deb8fc6aec760542df383809b3eb5239e
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-02 01:43:50 -0300

    feat: builder patter task

--- 01-creacionales/01.2-builder.ts
+++ 01-creacionales/01.2-builder.ts
@@ -50,24 +50,35 @@ class QueryBuilder {
   }
 
   select(...fields: string[]): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.fields = fields;
+    return this;
   }
 
   where(condition: string): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.conditions.push(condition);
+    return this;
   }
 
   orderBy(field: string, direction: 'ASC' | 'DESC' = 'ASC'): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.orderFields.push(`${field} ${direction}`);
+    return this;
   }
 
   limit(count: number): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.limitCount = count;
+    return this;
   }
 
   execute(): string {
     // Select id, name, email from users where age > 18 and country = 'Cri' order by name ASC limit 10;
-    throw new Error('Method not implemented.');
+    let query = "select ";
+    if (this.fields.length) query += this.fields.join(', ')
+    else query += ' *'
+    query += ` from ${this.table}`
+    if (this.conditions.length) query += ` where ${this.conditions.join(' and ')}`;
+    if (this.orderFields.length) query += ` order by ${this.orderFields.join(', ')}`;
+    if (this.limitCount) query += ` limit ${this.limitCount}`;
+    return query;
   }
 }
 
