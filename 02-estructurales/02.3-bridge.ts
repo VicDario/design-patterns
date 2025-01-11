54a15b451ef8ce7f7df827245eb36157433216d1
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-11 03:11:25 -0300

    feat: bridge pattern task 2

--- 02-estructurales/02.3-bridge.ts
+++ 02-estructurales/02.3-bridge.ts
@@ -9,3 +9,70 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+// 1. Interfaz NotificationChannel
+// Define el método `send`, que cada canal de comunicación implementará.
+interface NotificationChannel {
+  send(message: string): void;
+}
+
+// 2. Implementaciones de Canales de Comunicación
+
+class EmailChannel implements NotificationChannel {
+  send(message: string): void {
+    console.log(`Enviando correo electrónico: ${message}`);
+  }
+}
+
+class SMSChannel implements NotificationChannel {
+  send(message: string): void {
+    console.log(`Enviando SMS: ${message}`);
+  }
+}
+
+class PushNotificationChannel implements NotificationChannel {
+  send(message: string): void {
+    console.log(`Enviando Push: ${message}`);
+  }
+}
+
+// 3. Clase Abstracta Notification
+// Define la propiedad `channel` y el método `notify`
+
+abstract class Notification {
+  protected chanels: NotificationChannel[];
+  constructor(channel: NotificationChannel[]) {
+    this.chanels = channel;
+  }
+
+  abstract notify(message: string): void;
+  abstract addChannel(channel: NotificationChannel): void;
+}
+
+class AlertNotification extends Notification {
+  override notify(message: string): void {
+    console.log("\n%cNotificacion de alerta", COLORS.red);
+    this.chanels.forEach((channel) => channel.send(message));
+  }
+  override addChannel(channel: NotificationChannel): void {
+    this.chanels.push(channel);
+  }
+}
+
+function main() {
+  const channels = [
+    new EmailChannel(),
+    new SMSChannel(),
+    new PushNotificationChannel(),
+    new PushNotificationChannel(),
+    new PushNotificationChannel(),
+  ];
+
+  const alert = new AlertNotification(channels);
+
+  alert.notify("Alerta de seguridad: Alguien enfrente de la casa");
+}
+
+main();
