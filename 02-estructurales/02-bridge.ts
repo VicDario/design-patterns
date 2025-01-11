8426e662527ce08fbc34b36d741d3b59a53adebc
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-11 02:52:13 -0300

    feat: bridge pattern explanation

--- 02-estructurales/02-bridge.ts
+++ 02-estructurales/02-bridge.ts
@@ -9,3 +9,77 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+import { COLORS } from '../helpers/colors.ts';
+
+interface Ability {
+  use(): void;
+}
+
+class SwordAttack implements Ability {
+  use(): void {
+    console.log('Atacar con una %cespada ferozmente', COLORS.blue);
+  }
+}
+
+class MagicSpellAttack implements Ability {
+  use(): void {
+    console.log('Lanza un hechizo %cmagico poderoso', COLORS.green);
+  }
+}
+
+abstract class Character {
+  protected ability: Ability;
+
+  constructor(ability: Ability) {
+    this.ability = ability;
+  }
+
+  setAbility(ability: Ability): void {
+    this.ability = ability;
+  }
+
+  abstract perfomAbility(): void;
+}
+
+class Warrior extends Character {
+  override perfomAbility(): void {
+    console.log('El guerrero esta listo para lucar');
+    this.ability.use();
+  }
+}
+
+class Mage extends Character {
+  override perfomAbility(): void {
+    console.log('El mago prepara su magia');
+    this.ability.use();
+  }
+}
+
+class AxeAttack implements Ability {
+    use(): void {
+      console.log('Golpea con el %chacha salvajemente', COLORS.brown)
+    }
+}
+
+class FireBallSpell implements Ability {
+    use(): void {
+      console.log('Lanza una gran %cBola de fuego', COLORS.red);
+    }
+}
+
+function main() {
+    const warrior = new Warrior(new SwordAttack());
+    warrior.perfomAbility();
+
+    warrior.setAbility(new AxeAttack());
+    warrior.perfomAbility();
+
+    const mage = new Mage(new MagicSpellAttack());
+    mage.perfomAbility();
+
+    mage.setAbility(new FireBallSpell());
+    mage.perfomAbility();
+}
+
+main();
