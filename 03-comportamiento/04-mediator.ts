85dffc832d4c34528c24918df06e6739333ecce1
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-23 02:06:15 -0300

    feat: mediator explanation

--- 03-comportamiento/04-mediator.ts
+++ 03-comportamiento/04-mediator.ts
@@ -9,3 +9,71 @@
  *
  * https://refactoring.guru/es/design-patterns/mediator
  */
+
+import { COLORS } from '../helpers/colors.ts';
+
+// Chatroom
+
+class ChatRoom {
+  private users: User[] = [];
+  public title: string;
+
+  constructor(title: string) {
+    this.title = title;
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
+  }
+
+  sendMessage(sender: User, message: string) {
+    const usersToSend = this.users.filter((user) => user !== sender);
+    for (const user of usersToSend) {
+      user.receiveMessage(sender, message);
+    }
+  }
+}
+
+class User {
+  private username: string;
+  private chatRoom: ChatRoom;
+
+  constructor(username: string, chatRoom: ChatRoom) {
+    this.username = username;
+    this.chatRoom = chatRoom;
+    chatRoom.addUser(this);
+  }
+
+  sendMessage(message: string) {
+    console.log(
+      `\n%c${this.username} envia: %c${message}`,
+      COLORS.blue,
+      COLORS.white
+    );
+    this.chatRoom.sendMessage(this, message);
+  }
+
+  receiveMessage(sender: User, message: string) {
+    console.log(
+      `%c${this.username} recibe de ${sender.username}: %c${message}`,
+      COLORS.blue,
+      COLORS.white
+    );
+  }
+}
+
+function main() {
+  const chatRoom = new ChatRoom('Sala');
+
+  const user1 = new User('Victor', chatRoom);
+  const user2 = new User('Daniela', chatRoom);
+  const user3 = new User('Kevin', chatRoom);
+  const user4 = new User('José', chatRoom);
+
+  user1.sendMessage('Hola a todos');
+  user2.sendMessage('Hola, como están?');
+  user3.sendMessage('Excelente :D');
+  user4.sendMessage('8)');
+}
+
+main();
