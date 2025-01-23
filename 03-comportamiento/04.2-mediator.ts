d47946549f3a3b1a04668928c27b39850e1167c4
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-23 02:22:17 -0300

    feat: mediator task

--- 03-comportamiento/04.2-mediator.ts
+++ 03-comportamiento/04.2-mediator.ts
@@ -36,12 +36,15 @@ class ControlTower {
   private airplanes: Airplane[] = [];
 
   // Registrar un avión en la torre de control
-  // TODO: Implementar el método registerAirplane
-  // registerAirplane(airplane: Airplane)
+  registerAirplane(airplane: Airplane) {
+    this.airplanes.push(airplane);
+  }
 
   // Enviar un mensaje de un avión a todos los demás
-  //TODO: Implementar el método sendMessage
-  // sendMessage(sender: Airplane, message: string): void
+  sendMessage(sender: Airplane, message: string): void {
+    const sendTo = this.airplanes.filter(airplane => airplane !== sender);
+    sendTo.forEach(airplane => airplane.receiveMessage(sender, message));
+  }
 
   // Coordinación de aterrizaje
   requestLanding(sender: Airplane): void {
@@ -75,7 +78,7 @@ class Airplane {
     this.id = id;
     this.controlTower = controlTower;
 
-    // TODO: Registrar el avión en la torre de control
+    controlTower.registerAirplane(this);
   }
 
   getId(): string {
@@ -86,14 +89,17 @@ class Airplane {
   requestLanding(): void {
     console.log(`${this.id} solicita permiso para aterrizar.`);
 
-    // TODO: Solicitar aterrizaje a la torre de control
+    // Solicitar aterrizaje a la torre de control
+    this.controlTower.requestLanding(this);
+
   }
 
   // Solicitar despegue a la torre de control
   requestTakeoff(): void {
     console.log(`${this.id} solicita permiso para despegar.`);
 
-    // TODO: Solicitar despegue a la torre de control
+    // Solicitar despegue a la torre de control
+    this.controlTower.requestTakeoff(this);
   }
 
   // Recibir mensaje de otros aviones
