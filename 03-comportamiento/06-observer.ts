7acb602b741991416ac573b505f77027bba987c9
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-29 01:45:02 -0300

    feat: observer explanation

--- 03-comportamiento/06-observer.ts
+++ 03-comportamiento/06-observer.ts
@@ -13,3 +13,79 @@
  *
  * https://refactoring.guru/es/design-patterns/observer
  */
+
+import { COLORS } from '../helpers/colors.ts';
+
+interface Observer {
+  notify(videoTitle: string): void;
+}
+
+class YoutubeChannel {
+  private subscribers: Observer[] = [];
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  subscribe(observer: Observer) {
+    this.subscribers.push(observer);
+    console.log(`Nuevo suscriptor al canal %c${this.name}`, COLORS.green);
+  }
+
+  unsubscribe(observer: Observer) {
+    this.subscribers = this.subscribers.filter(
+      (subscriber) => subscriber !== observer
+    );
+    console.log(
+      `Un suscriptor se ha dado de baja %c"${this.name}"`,
+      COLORS.red
+    );
+  }
+
+  uploadVideo(videoTitle: string) {
+    console.log(
+      `Canal ${this.name} ha subido un nuevo video %c${videoTitle}`,
+      COLORS.green
+    );
+
+    for (const subscriber of this.subscribers) subscriber.notify(videoTitle);
+  }
+}
+
+class Subscriber implements Observer {
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  notify(videoTitle: string): void {
+    console.log(
+      `${this.name} ha sido notificado: %cNuevo video ${videoTitle}`,
+      COLORS.blue
+    );
+  }
+}
+
+function main() {
+  const channel = new YoutubeChannel('Jueguitos');
+  const melissa = new Subscriber('Melissa');
+  const cesar = new Subscriber('César');
+  const emin = new Subscriber('Emin');
+
+  channel.subscribe(melissa);
+  channel.subscribe(cesar);
+
+  channel.uploadVideo('Hell Let Loose');
+
+  channel.subscribe(emin);
+
+  channel.uploadVideo('Delta Force');
+
+  channel.unsubscribe(cesar);
+
+  channel.uploadVideo('Balatro');
+}
+
+main();
