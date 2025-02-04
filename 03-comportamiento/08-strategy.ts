2f25eb85f7d75ecef7f1dafacb0b664a628eb2e5
VicDario <26585356+VicDario@users.noreply.github.com>
2025-02-03 23:14:59 -0300

    feat: strategy pattern explanation

--- 03-comportamiento/08-strategy.ts
+++ 03-comportamiento/08-strategy.ts
@@ -12,8 +12,75 @@
  * https://refactoring.guru/es/design-patterns/strategy
  */
 
+import { COLORS } from '../helpers/colors.ts';
+
 /**
  * !Objetivo: Explicar el patrón Strategy usando un ejemplo donde varios
  * ! patitos compiten en una carrera y cada uno tiene su propia
  * ! estrategia de movimiento (por ejemplo, nadar, volar o caminar).
  */
+
+interface MovementStrategy {
+  move(): void;
+}
+
+// Estrategia #1 - Rápida pero costosa
+class SwimFast implements MovementStrategy {
+  move(): void {
+    console.log('%cEl pato nada rápidamente sobre el agua\n', COLORS.blue);
+  }
+}
+
+// Estrategia #2 - No tan rápida pero no tan costosa
+class FlyOverWater implements MovementStrategy {
+  move(): void {
+    console.log('%cEl pato vuela elegantemente sobre el agua\n', COLORS.pink);
+  }
+}
+
+// Estrategia #3 - Lenta y económica
+class WalkClumsily implements MovementStrategy {
+  move(): void {
+    console.log('%cEl pato camina torpemente por la orilla\n', COLORS.green);
+  }
+}
+
+// Consumidor de estrategias
+class Duck {
+  private name: string;
+  private movementStrategy: MovementStrategy;
+
+  constructor(name: string, strategy: MovementStrategy) {
+    this.name = name;
+    this.movementStrategy = strategy;
+
+    console.log(`%c${name} %clisto para competir`, COLORS.green, COLORS.white);
+  }
+
+  performMovement() {
+    console.log(`${this.name} se prepara para moverse...`);
+    this.movementStrategy.move();
+  }
+
+  setMovementStrategy(strategy: MovementStrategy) {
+    this.movementStrategy = strategy;
+    console.log(`${this.name} cambio de estrategia.`);
+  }
+}
+
+function main() {
+  const duck1 = new Duck('Patito rápido', new SwimFast());
+  const duck2 = new Duck('Patito volador', new FlyOverWater());
+  const duck3 = new Duck('Patito torpe', new WalkClumsily());
+
+  console.log('%cComienza la carrera de patos\n', COLORS.red);
+
+  duck1.performMovement();
+  duck2.performMovement();
+  duck3.performMovement();
+
+  duck3.setMovementStrategy(new FlyOverWater());
+  duck3.performMovement();
+}
+
+main();
